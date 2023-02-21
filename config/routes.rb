c305622ae3a6bbea2688e899ec6e8907ca350841
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-02-22 00:15:33 +0100

    Add shots controller for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :shots
+  devise_for :users, controllers: { registrations: 'registrations'}
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
