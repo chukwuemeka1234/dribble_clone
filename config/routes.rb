ca52006204db452fd3217ebdceff1ff1eed9249e
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-02-22 23:41:55 +0100

    Make change on the shot view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :shots
   devise_for :users, controllers: { registrations: 'registrations'}
-  root 'home#index'
+  root 'shots#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
