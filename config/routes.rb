f9821a67b66fb0761feb39410551ae574fb430f1
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2023-03-03 22:13:57 +0100

    Add the css for application design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :shots
+
   devise_for :users, controllers: { registrations: 'registrations' }
+  
   root 'shots#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
